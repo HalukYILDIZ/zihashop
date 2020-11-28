140cc2a5da82a9425678132a4977ece88dfd1d05
Haluk Yıldız <halukyildiz@Haluk-MacBook-Air.local>
2020-11-28 16:47:00 +0300

    yeni ürünler eklendi

--- src/services/dummy.js
+++ src/services/dummy.js
@@ -23,4 +23,28 @@ export const dummyProducts = [
     price: 2099.90,
     photo: "/img/3.png"
   },
+  {
+    id: 5,
+    name: "DJI Agras MG-1S Tohumlama Kiti",
+    price: 1099.00,
+    photo: "/img/5.png",
+  },
+  {
+    id: 6,
+    name: "DJI Agras MG-1P | Akıllı Pil Şarj Cihazı",
+    price: 1999.00,
+    photo: "/img/6.jpg"
+  },
+  {
+    id: 7,
+    name: "DJI Agras T16 Akıllı Uçuş Bataryası",
+    price: 1649.90,
+    photo: "/img/7.jpg"
+  },
+  {
+    id: 8,
+    name: "DJI Agras MG-12000P Akıllı Uçuş Bataryası",
+    price: 1098.00,
+    photo: "/img/8.png"
+  },
 ];
