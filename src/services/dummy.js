12b1ac89ce96020f491ba5344986b7a8353d3c7c
Haluk Yıldız <halukyildiz@Haluk-MacBook-Air.local>
2020-12-09 22:53:25 +0300

    bug fixed

--- src/services/dummy.js
+++ src/services/dummy.js
@@ -2,52 +2,52 @@ export const dummyProducts = [
   {
     id: 1,
     name: "DJI AGRAS T16",
-    price: 19799.00,
+    price: 19799.0,
     photo: "/img/1.png",
   },
   {
     id: 2,
     name: "DJI AGRAS MG1-P",
-    price: 17598.00,
-    photo: "/img/2.jpg"
+    price: 17598.0,
+    photo: "/img/2.jpg",
   },
   {
     id: 3,
     name: "DJI PHANTOM 4",
-    price: 1599.90,
-    photo: "/img/4.png"
+    price: 1599.9,
+    photo: "/img/4.png",
   },
   {
     id: 4,
     name: "DJI AGRAS T16 Akıllı Şarj Cihazı",
-    price: 2099.90,
-    photo: "/img/3.png"
+    price: 2099.9,
+    photo: "/img/3.png",
   },
   {
     id: 5,
     name: "DJI Agras MG-1S Tohumlama Kiti",
-    price: 1099.00,
+    price: 1099.0,
     photo: "/img/5.png",
   },
   {
     id: 6,
     name: "DJI Agras MG-1P | Akıllı Pil Şarj Cihazı",
-    price: 1999.00,
-    photo: "/img/6.jpg"
+    price: 1999.0,
+    photo: "/img/6.jpg",
   },
   {
     id: 7,
     name: "DJI Agras T16 Akıllı Uçuş Bataryası",
-    price: 1649.90,
-    photo: "/img/7.jpg"
+    price: 1649.9,
+    photo: "/img/7.jpg",
   },
   {
     id: 8,
     name: "DJI Agras MG-12000P Akıllı Uçuş Bataryası",
-    price: 1098.00,
-    photo: "/img/8.png"
+    price: 1098.0,
+    photo: "/img/8.png",
   },
-    {
+  {
     id: 9,
     name: "DJI Matrice 200 V2.0 Drone Seti",
     price: 6898.31,
@@ -56,19 +56,19 @@ export const dummyProducts = [
   {
     id: 10,
     name: "DJI Matrice 200 TB55 Batarya Part 03",
-    price: 600.00,
-    photo: "/img/10.jpg"
+    price: 600.0,
+    photo: "/img/10.jpeg",
   },
   {
     id: 11,
     name: "DJI Matrice 210 V2.0 Drone Seti",
     price: 12288.14,
-    photo: "/img/11.png"
+    photo: "/img/11.png",
   },
   {
     id: 12,
     name: "DJI Matrice 600 Pro Drone Seti",
-    price: 7530.00,
-    photo: "/img/12.jpg"
+    price: 7530.0,
+    photo: "/img/12.jpg",
   },
 ];
