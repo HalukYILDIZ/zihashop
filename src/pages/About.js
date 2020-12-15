fc7b20b7a74c66ae391d790971838961ea4d41db
halukyildiz <halukyildiz4000@gmail.com>
2020-12-15 21:04:01 +0300

    icon changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import { GitHubIcon} from '../components/icons'
+import React from "react";
+import Layout from "../components/Layout";
+import { GitHubIcon } from "../components/icons";
 
 const About = () => {
-    
-    return ( 
-        <Layout title="About" description="This is the About page" >
-            <div className="text-center mt-5">
-                <h1>Hakkında</h1>
-                <p>Tüm ürünlerimizi elden, kontrol ederek ve eğitimlerini vererek teslim ediyoruz.</p>
+  return (
+    <Layout title="About" description="This is the About page">
+      <div className="text-center mt-5">
+        <h1>Hakkında</h1>
+        <p>
+          Tüm ürünlerimizi elden, kontrol ederek ve eğitimlerini vererek teslim
+          ediyoruz.
+        </p>
 
-                <a className="btn btn-primary" href="https://www.zihatim.com">
-                    <GitHubIcon width={"18px"}/> <span className="ml-2 mr-4">Anasayfaya dön </span></a>
-            </div>
-        </Layout>
-     );
-}
- 
+        <a className="btn btn-primary" href="https://www.zihatim.com">
+          <span className="ml-2 mr-4">Anasayfaya dön </span>
+        </a>
+      </div>
+    </Layout>
+  );
+};
+
 export default About;
